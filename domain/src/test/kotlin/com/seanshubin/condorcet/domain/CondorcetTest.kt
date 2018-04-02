3db1aa2a4c243618fd1fbc2237372126bb15cb06
Sean Shubin <sshubin@cj.com>
2018-04-02 14:43:28 -0700

    made data format have a predictable order

--- domain/src/test/kotlin/com/seanshubin/condorcet/domain/CondorcetTest.kt
+++ domain/src/test/kotlin/com/seanshubin/condorcet/domain/CondorcetTest.kt
@@ -4,22 +4,14 @@ import org.junit.Test
 
 class CondorcetTest {
     @Test
-    fun cycleSample() {
-        val text = """
-            |5 acbed
-            |5 adecb
-            |8 bedac
-            |3 cabed
-            |7 caebd
-            |2 cbade
-            |7 dceba
-            |8 ebadc
-            """.trimMargin()
-        val candidatesAndVotes = CandidatesAndVotes.parseFromQuantifiedSingleLetterCandidates(text)
-        val schulze = candidatesAndVotes.tally()
-        println(candidatesAndVotes)
-//        val election = Election.createFromQuantifiedSingleLetterCandidates(text)
-//        val tally = election.tally()
-//        tally.reportLines().foreach(println)
+    fun testDataSamples() {
+        runTest("01-spoiler")
+        runTest("02-tactical-voting")
+        runTest("03-schulze-example-from-wikipedia")
+    }
+
+    fun runTest(name: String): Unit {
+
+//        assertLinesEqual(actualLines, expectedLines)
     }
 }
--- prototype/src/main/kotlin/com/seanshubin/condorcet/prototype/NameGeneratorApp.kt
+++ prototype/src/main/kotlin/com/seanshubin/condorcet/prototype/NameGeneratorApp.kt
@@ -1,5 +1,5 @@
 package com.seanshubin.condorcet.prototype
 
 public fun main(args: Array<String>) {
-    (1..50).forEach { println(DefaultNameGenerator.generateName()) }
+    (1..50).map { DefaultNameGenerator.generateName() }.sorted().forEach { println(it) }
 }
